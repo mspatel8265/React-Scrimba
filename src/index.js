07bc6db8e0ddd3f41f3c3f76c7955cea9709fa95
Margi Patel <mspatel.8265@gmail.com>
2019-02-13 00:26:35 -0500

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// // ReactDOM.render(WHAT DO I WANT TO RENDER, WHERE DO I WANT TO RENDER);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ReactDOM.render(
+//   <div>
+//     <h1>Hello World</h1>
+//     <p>This is a paragraph.</p>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+function MyApp() {
+  return (
+    <ul>
+      <li>Margi</li>
+      <li>An expert coder</li>
+      <li>A kind person</li>
+    </ul>
+  );
+}
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Margi Patel</h1>
+      <p>
+        I am a software engineer and I love learning new technologies. Currently
+        I am learning React, unfortunatley I haven't been able to develop
+        anything or finish anything of my project but that's not the end, I will
+        work really hard to finish my all the assignments.
+      </p>
+      <ul>
+        <li>Switzerland - land of wonderful chocolates.</li>
+        <li>Paris - love destination, after learning French.</li>
+        <li>London - visit my brother.</li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
